7a519432d04f11cf01a56ccf52d0c8ac81fd3b53
Son Nguyen <son.nguyen@optimizely.com>
2022-06-01 15:16:24 +0700

    Simulate running an async operation in UI thread with AsyncContext

---
+++ Library.NetFramework/AsyncContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library.NetFramework
+{
+    public static class AsyncContext
+    {
+        public static void Run(Func<Task> func)
+        {
+            var originalSyncContext = SynchronizationContext.Current;
+
+            try
+            {
+                var uiThreadSyncContext = new PseudoUIThreadSynchronizationContext();
+                SynchronizationContext.SetSynchronizationContext(uiThreadSyncContext);
+
+                var task = func();
+                task.ContinueWith(_ => uiThreadSyncContext.Complete());
+
+                uiThreadSyncContext.StartConsumingMessage();
+
+                task.GetAwaiter().GetResult();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(originalSyncContext);
+            }
+        }
+
+        private class PseudoUIThreadSynchronizationContext : SynchronizationContext
+        {
+            private readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object>> _workQueue;
+
+            public PseudoUIThreadSynchronizationContext()
+            {
+                _workQueue = new BlockingCollection<KeyValuePair<SendOrPostCallback, object>>();
+            }
+
+            public void StartConsumingMessage()
+            {
+                foreach (var message in _workQueue.GetConsumingEnumerable())
+                {
+                    message.Key(message.Value);
+                }
+            }
+
+            public void Complete() => _workQueue.CompleteAdding();
+
+            public override void Post(SendOrPostCallback d, object state)
+            {
+                _workQueue.Add(new KeyValuePair<SendOrPostCallback, object>(d, state));
+            }
+        }
+    }
+}
